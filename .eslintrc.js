5279d154160bf70a5f0ab548cf457ad62d43c99a
Sjk824 <471572362@qq.com>
2017-09-17 21:39:38 +0800

    调整配置参数，设置基础配置和样式

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,11 +17,27 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
-    'arrow-parens': 0,
-    // allow async-await
-    'generator-star-spacing': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+      // allow paren-less arrow functions
+      'arrow-parens': 0,
+      // allow async-await
+      'generator-star-spacing': 0,
+      // allow debugger during development
+      'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+      'comma-spacing': 0,
+      'comma-style': 0,
+      'semi': [2, 'always'],
+      'semi-spacing': 0,
+      'semi-style': 0,
+      'indent': 0,
+      'keyword-spacing': 0,
+      'space-before-blocks': 0,
+      'space-before-function-paren': 0,
+      'space-in-parens': 0,
+      'space-infix-ops': 0,
+      'space-unary-ops': 0,
+      'spaced-comment': 0,
+      'no-ternary': 0,
+      'no-plusplus': 0,
+      'one-var': 0
   }
 }
