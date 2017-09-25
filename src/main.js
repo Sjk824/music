ab334dc61840d78765bd163ee67212ba1fbc2036
Sjk824 <471572362@qq.com>
2017-09-25 16:55:30 +0800

    引入vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import 'babel-polyfill';
 import Vue from 'vue';
 import App from './App';
 import router from './router/router';
+import store from './store/index';
 import fastclick from 'fastclick';
 import VueLazyload from 'vue-lazyload';
 
@@ -17,6 +18,7 @@ Vue.use(VueLazyload, {
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 });
